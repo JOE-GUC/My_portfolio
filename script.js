0b9e440b9e465feff4f7024652aa1f20a3f22ea3
JOSEPH-JOSE <joeboy65458@agmail.com>
2024-06-07 16:24:22 +0100

    done

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const carousel = document.querySelector('.carousel-container');
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${100 * (i - index)}%)`;
+    });
+    currentSlide = index;
+}
+
+// Previous and Next buttons functionality
+document.querySelector('#prev').addEventListener('click', () => {
+    if (currentSlide > 0) {
+        showSlide(currentSlide - 1);
+    }
+});
+
+document.querySelector('#next').addEventListener('click', () => {
+    if (currentSlide < slides.length - 1) {
+        showSlide(currentSlide + 1);
+    }
+});
+
+// Initial slide
+showSlide(0);
+
+
 function toggleMenu() {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
